5ca0b4ce88ddcdbd6c28154d5112e1e39235f149
onekill0503 <gamemasteraji123@gmail.com>
2021-09-29 11:31:07 +0700

    Initial Commit for master branch

---
+++ app/src/main/java/id/project195410083/aji/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.project195410083.aji
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
